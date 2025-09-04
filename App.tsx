dcebe48a2460bc7d844ac282d85eee40c6aa2e53
Elizandro Galho <111243774+Elizandro2022@users.noreply.github.com>
2025-09-04 10:59:03 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/pages/login';
+
+ interface corda{
+    nome: string;
+    idade: number;
+    
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+       <Login/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+  
+});
